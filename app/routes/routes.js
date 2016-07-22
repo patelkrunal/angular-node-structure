b5ce07d9c0bbb07c16578c29cb5fc834725a4c97
Krunal <krunal@Krunals-MBP.fios-router.home>
2016-07-22 19:32:46 -0400

    Initial commit for angulat-node structure

---
+++ app/routes/routes.js
@@ -0,0 +1,36 @@
+/**
+ * Created by krunal on 11/3/15.
+ */
+
+//require everything that you need.
+var router = require('express').Router();
+var Indicative = new(require("indicative"));
+var _          = require('underscore');
+
+
+    // middleware to use for all requests
+    router.use(function(req, res, next) {
+        // do logging
+        next();
+    });
+
+    // test route to make sure everything is working (accessed at GET http://host:port/api)
+    router.get('/', function(req, res) {
+        res.json({ message: 'Welcome to Krunal API' });
+    });
+
+
+    // ROUTES FOR OUR API
+    // =============================================================================
+
+    // on routes that end in /user
+    // ----------------------------------------------------
+
+
+
+    router.route('/user')
+        .get(function(req, res) {
+            res.json({message:"USER API"});
+        });
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,47 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+var morgan     = require('morgan');
+var _          = require('underscore');
+
+//host and port from cli
+var hostIndex = _.indexOf(process.argv,"--host");
+if(hostIndex > -1){
+    if(process.argv[hostIndex+1])
+        process.env.HOST = process.argv[hostIndex+1];
+}
+var portIndex = _.indexOf(process.argv,"--port");
+if(portIndex > -1){
+    if(process.argv[portIndex+1])
+        process.env.PORT = process.argv[portIndex+1];
+}
+
+// configure app
+app.use(morgan('dev')); // log requests to the console
+
+// configure body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 8080);// set default port
+app.set('host', process.env.HOST || 'localhost');//set default host
+
+//add all the routes.
+var router = require('./app/routes/routes');
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(app.get('port'), function(){
+    console.log("Express server listening at %s:%s ",app.get('host'),app.get('port'));
+});
